6940d63c621c8c6321203f7aa9f03a0e2f71bc34
xuyongkang <2200126769@qq.com>
2025-03-29 22:11:53 +0800

    最终版

--- extension.js
+++ extension.js
@@ -5,30 +5,33 @@ function activate(context) {
     vscode.commands.registerCommand("validate", () => {
       vscode.window.showInformationMessage("it works")
     }),
-    vscode.commands.registerCommand("swapFilenames", async () => {
-      const selectedFiles = await vscode.window.showOpenDialog({
-        canSelectMany: true,
-        openLabel: "Select 2 files to swap names"
-      });
+    vscode.commands.registerCommand("swapFilenamesBetter", async(uri, selectedUris) => {
+        // 获取当前在资源管理器中选中的文件
+        let filesToSwap = [];
       
-      if (!selectedFiles || selectedFiles.length !== 2) {
-        vscode.window.showErrorMessage("Please select exactly 2 files");
-        return;
-      }
-
-      const [file1, file2] = selectedFiles;
-      const file1Path = file1.fsPath;
-      const file2Path = file2.fsPath;
-      const tempPath = file1Path + ".temp";
-
-      try {
-        await vscode.workspace.fs.rename(file1, vscode.Uri.file(tempPath));
-        await vscode.workspace.fs.rename(file2, vscode.Uri.file(file1Path));
-        await vscode.workspace.fs.rename(vscode.Uri.file(tempPath), vscode.Uri.file(file2Path));
-        vscode.window.showInformationMessage("Files renamed successfully!");
-      } catch (error) {
-        vscode.window.showErrorMessage(`Error swapping files: ${error.message}`);
-      }
+        // 如果有传入的选中文件，则使用它们
+        if (selectedUris && selectedUris.length === 2) {
+          filesToSwap = selectedUris;
+        } 
+        // 否则弹出提示
+        else {
+          vscode.window.showErrorMessage("请选择恰好两个文件");
+          return;
+        }
+  
+        const [file1, file2] = filesToSwap;
+        const file1Path = file1.fsPath;
+        const file2Path = file2.fsPath;
+        const tempPath = file1Path + ".temp";
+  
+        try {
+          await vscode.workspace.fs.rename(file1, vscode.Uri.file(tempPath));
+          await vscode.workspace.fs.rename(file2, vscode.Uri.file(file1Path));
+          await vscode.workspace.fs.rename(vscode.Uri.file(tempPath), vscode.Uri.file(file2Path));
+          vscode.window.showInformationMessage(`成功交换文件名: ${file1Path.split('/').pop()} ↔ ${file2Path.split('/').pop()}`);
+        } catch (error) {
+          vscode.window.showErrorMessage(`交换文件名时出错: ${error.message}`);
+        }
     })
   )
 }
